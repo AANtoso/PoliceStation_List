c0471739969d884a4f108597229b0a9137c258a5
Adjoa Ntoso <adjoaan@gmail.com>
2019-08-07 21:54:54 +0000

    Commented out part of Scraper.rb file.

--- lib/Scraper.rb
+++ lib/Scraper.rb
@@ -5,13 +5,15 @@ class PoliceStationScraper
   BASE_URL = 'https://www.policeone.com/law-enforcement-directory/'
 
   def self.scrape_policestationlist
-    pg = open(BASE_URL)
-    parsed_html = Nokogiri.HTML(pg)
-    station_list =
-    parsed_html.css ("a")
-    station_list.each do |a|
-      name: a.css('Table-row Table-row--highlight a').text.strip,
-      url: a.css('a')[0].attr('href')
+    # pg = open(BASE_URL)
+    # parsed_html = Nokogiri.HTML(pg)
+    # station_list =
+    # parsed_html.css ("a")
+    # station_list.each do |a|
+    #   name: a.css('Table-row Table-row--highlight a').text.strip,
+    #   url: a.css('a')[0].attr('href')
+    # end
+  end
   #  html = open("https://www.policeone.com/law-enforcement-directory/")
   #  doc = Nokogiri::HTML(html)
 
@@ -26,5 +28,4 @@ class PoliceStationScraper
     #def self.scrape_policestation_specifications(policestation)
     #  html = open()
     #end
-
   end
